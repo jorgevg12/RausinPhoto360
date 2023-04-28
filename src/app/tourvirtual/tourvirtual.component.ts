ca0ab12d52c237f30710d130e1164aa9b32a4f2f
jorgevg12 <jorgeguios@gmail.com>
2023-04-28 12:42:58 +0200

    last push

--- src/app/tourvirtual/tourvirtual.component.ts
+++ src/app/tourvirtual/tourvirtual.component.ts
@@ -13,8 +13,8 @@ export class TourVirtualComponent {
 tours: Tour[] = TOURS;
 
 onSelect(tour){
-    if(tour == 'infantas') window.open('https://view.ricohtours.com/aaf1ba0f-8631-49d9-8fda-3ace90065eb9');
-    if(tour == 'lemos') window.open('https://view.ricohtours.com/38271dfd-3917-4100-953b-6f427ebd08c7');
+    if(tour == 'arganzuelas') window.open('https://vt.plushglobalmedia.com/tour/TT17KVHE81');
+    if(tour == 'lemos') window.open('https://vt.plushglobalmedia.com/tour/TT17KL14LZ');
 }
 
 }
